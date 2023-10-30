1c0cb3d5dd3a38553a24c0524e540157f64064fb
Vadim <59086091+1Valerion1@users.noreply.github.com>
2023-10-31 01:20:26 +0700

    Add files via upload

--- Laba2/script2.sh
+++ Laba2/script2.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-# Ожидание одной секунды
-sleep 1
+# Файл с блокировкой для синхронизации
+lockfile="/shared_volume/sync.lock"
+
+# Создание блокировки для синхронизации
+exec {lock_fd}>$lockfile || exit 1
+
+# Получение эксклюзивной блокировки
+flock -x "$lock_fd" || exit 1
 
 # Определение незанятого имени
-flock /Desktop/Unix/shared_volume/sync.lock sh -c "
-   # Определение имени файла
-   number=1
-   while [ -f /Desktop/Unix/shared_volume/$(printf "%03d" "$number") ]; do
-   ((number++))
-   done
-   filename=$(printf "%03d" "$number")
- 
-   # Запись идентификатора контейнера и порядкового номера в файл
-   container_id=$(hostname)
-   content="$container_id $number"
-   echo "$content" > /Desktop/Unix/shared_volume/$filename
-  
-   # Задержка одну секунду перед удалением файла
-   sleep 1
-   
-   # Удаление файла
-   rm -fr /Desktop/Unix/shared_volume/$filename
-"
+counter_file="/shared_volume/counter.txt"
+if ! counter=$(cat "$counter_file" 2>/dev/null); then
+  counter=1
+else
+  ((counter++))
+fi
+echo "$counter" > "$counter_file"
+filename=$(printf "%03d" "$counter")
+
+# Запись идентификатора контейнера и порядкового номера в файл
+container_id=$(hostname)
+content="$container_id $counter"
+echo "$content" > "/shared_volume/$filename.txt"
+
+# Задержка одну секунду перед удалением файла
+sleep 1
+
+# Удаление файла
+if [ -n "$filename" ]; then
+  echo "Содержимое файла $filename:"
+  cat "/shared_volume/$filename.txt"
+  rm -fr "/shared_volume/$filename.txt"
+  echo "Файл $filename успешно удален."
+fi
+
+# Снятие блокировки
+flock -u "$lock_fd"
+exec {lock_fd}>&-
 

2a619959edf055be335447dabd9125f0fa7ad763
Vadim <59086091+1Valerion1@users.noreply.github.com>
2023-11-17 15:12:05 +0700

    Update script2.sh

--- Laba2/script2.sh
+++ Laba2/script2.sh
@@ -3,19 +3,23 @@
 # Файл с блокировкой для синхронизации
 lockfile="/shared_volume/sync.lock"
 
+while true; do
 # Создание блокировки для синхронизации
 exec {lock_fd}>$lockfile || exit 1
-
 # Получение эксклюзивной блокировки
 flock -x "$lock_fd" || exit 1
 
 # Определение незанятого имени
-counter_file="/shared_volume/counter.txt"
-if ! counter=$(cat "$counter_file" 2>/dev/null); then
-  counter=1
-else
-  ((counter++))
+counter_file="/shared_volume/count.txt"
+counter=$(printf "%03d" $((
+10#$(flock -x "$lock_fd"; cat "$counter_file" 2>/dev/null || echo 0) + 1
+)))
+
+# Обнуление счетчика при достижении 1000
+if [ $counter -ge 1000 ]; then
+    counter=0
 fi
+
 echo "$counter" > "$counter_file"
 filename=$(printf "%03d" "$counter")
 
@@ -23,6 +27,10 @@ filename=$(printf "%03d" "$counter")
 container_id=$(hostname)
 content="$container_id $counter"
 echo "$content" > "/shared_volume/$filename.txt"
+
+# Снятие блокировки
+flock -u "$lock_fd"
+exec {lock_fd}>&-
 
 # Задержка одну секунду перед удалением файла
 sleep 1
@@ -35,7 +43,6 @@ if [ -n "$filename" ]; then
   echo "Файл $filename успешно удален."
 fi
 
-# Снятие блокировки
-flock -u "$lock_fd"
-exec {lock_fd}>&-
+done
+
 
